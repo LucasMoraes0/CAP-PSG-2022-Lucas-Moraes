a5735bc0fde55fafddf9a224b0d44506de081cae
LucasMoraes0 <103219408+LucasMoraes0@users.noreply.github.com>
2022-04-08 16:17:39 -0400

    Create CREATE TABLE CATEGORIA .sql

---
+++ CREATE TABLE CATEGORIA .sql
@@ -0,0 +1,8 @@
+CREATE TABLE Categoria (
+	ID_Categoria INT NOT NULL IDENTITY(1,1),
+	Descricao_Categoria VARCHAR(MAX) NOT NULL,
+	Situacao BIT NULL DEFAULT 1,
+	DataInclusao DATETIME NOT NULL DEFAULT GETDATE(),
+	CONSTRAINT PK_categoria PRIMARY KEY (ID_Categoria)
+)
+GO
